a44f342444ffbefcf6f8225dadeaaaea5b962a7e
Abhishek Gayakwad <a.gayakwad@gmail.com>
2017-04-09 21:09:56 -0700

    changed repo url from https to ssh

--- .teamcity/_Root/vcsRoots/RootProjectConfig.kt
+++ .teamcity/_Root/vcsRoots/RootProjectConfig.kt
@@ -6,8 +6,8 @@ object RootProjectConfig : GitVcsRoot({
     uuid = "597cf052-5365-4e26-a674-7fc4e3a105bb"
     extId = "RootProjectConfig"
     name = "RootProjectConfig"
-    url = "https://github.com/gayakwad/teamcity-root-config.git"
-    pushUrl = "https://github.com/gayakwad/teamcity-root-config.git"
+    url = "git@github.com:gayakwad/teamcity-root-config.git"
+    pushUrl = "git@github.com:gayakwad/teamcity-root-config.git"
     authMethod = uploadedKey {
         uploadedKey = "VSC Key"
     }
